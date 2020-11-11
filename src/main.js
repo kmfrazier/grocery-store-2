54b446be7d1b65ca584692b9a03e481469a85f83
Kyle Frazier <kyle.matthew.frazier@gmail.com>
2020-11-10 17:08:31 -0700

    finished lab 2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import mock from './mock-data.js'
+
+
+
+let data = {
+  products: mock,
+  cart: []
+}
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  data,
   render: h => h(App)
 }).$mount('#app')
